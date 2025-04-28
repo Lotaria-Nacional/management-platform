0365c9a3b6a4697319db9fcc95640e3dfb909573
Paulods0 <pauloluguenda0@gmail.com>
2025-04-28 11:59:12 +0100

    initial config

--- backend/src/domain/file/application/interface/agent.repository.ts
+++
@@ -1,5 +0,0 @@
-import { Agent } from "@/domain/file/enterprise/entities/agent.entity";
-
-export interface AgentRepository {
-    save: (agent:Agent)=> Promise<void>
-}
--- backend/src/domain/file/application/interface/file-reader.interface.ts
+++
@@ -1,5 +0,0 @@
-import {  ParsedRowDTO } from "../../enterprise/dtos/parsed-row.dto";
-
-export interface IFileReader {
-    read(file:Buffer):Promise<ParsedRowDTO[]>
-}
