d1038b2baf1858d228615e97958dcdd5ddb3afdd
Paulods0 <pauloluguenda0@gmail.com>
2025-05-02 04:05:45 +0100

    commit check

--- backend/src/domain/terminal/application/use-cases/fetch-terminal.useCase.ts
+++ backend/src/domain/terminal/application/use-cases/fetch-terminal.useCase.ts
@@ -1,13 +1,12 @@
-import { ITerminalRepository } from "../interfaces/terminal-repository.interface";
-import { FetchTerminalsUseCaseResponseDto } from "../dto/fetch-terminals-use-case-request-dto";
+import { ITerminalRepository } from "../interfaces/terminal-repository.interface"
+import { FetchTerminalsUseCaseResponseDto } from "../dto/fetch-terminals-use-case-request-dto"
 
 export class FetchTerminalsUseCase {
-    constructor(private terminalRepository:ITerminalRepository){}
+  constructor(private terminalRepository: ITerminalRepository) {}
 
-    async execute():Promise<FetchTerminalsUseCaseResponseDto>{
- 
-        const terminals = await this.terminalRepository.fetchAll()
+  async execute(): Promise<FetchTerminalsUseCaseResponseDto> {
+    const terminals = await this.terminalRepository.fetchAll()
 
-        return { terminals }
-    }
+    return { terminals }
+  }
 }
--- backend/src/domain/terminal/presentation/controllers/fetch-terminal.controller.ts
+++ backend/src/domain/terminal/presentation/controllers/fetch-terminal.controller.ts
@@ -1,27 +1,29 @@
-import { HttpRequest, HttpResponse, IController } from "@/core/presentation/http";
-import { FetchTerminalsUseCase } from "../../application/use-cases/fetch-terminal.useCase";
+import {
+  HttpRequest,
+  HttpResponse,
+  IController,
+} from "@/core/presentation/http"
+import { FetchTerminalsUseCase } from "../../application/use-cases/fetch-terminal.useCase"
 
 export class FetchTerminalsController implements IController {
   constructor(private useCase: FetchTerminalsUseCase) {}
 
   async handle(_request: HttpRequest): Promise<HttpResponse> {
-
     try {
-      const { terminals } = await this.useCase.execute();
+      const { terminals } = await this.useCase.execute()
 
       return {
         statusCode: 201,
-        body: terminals
-      };
-      
+        body: terminals,
+      }
     } catch (error: any) {
       return {
         statusCode: 500,
         body: {
           message: "Internal server error",
-          error: error.message || error
-        }
-      };
+          error: error.message || error,
+        },
+      }
     }
   }
 }
