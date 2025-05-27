466a743fe04eb366237a6e4dd15c7b8b8ead467a
Paulods0 <pauloluguenda0@gmail.com>
2025-05-27 02:44:48 +0100

    add seed data on cities

--- backend/src/core/database/seed.ts
+++ backend/src/core/database/seed.ts
@@ -3,10 +3,65 @@ import { PrismaClient } from "generated/prisma"
 const prisma = new PrismaClient()
 
 async function main() {
-  await prisma.administration.create({
-    data: {
-      name: 12345,
-    },
+  await prisma.city.createMany({
+    data: [
+      {
+        province_id: "68308efdaf02223900052ee8",
+        name: "Cacuso",
+      },
+      {
+        province_id: "68308efdaf02223900052ee8",
+        name: "Caombo",
+      },
+      {
+        province_id: "68308efdaf02223900052ee8",
+        name: "Calandula",
+      },
+      {
+        province_id: "68308efdaf02223900052ee8",
+        name: "Cambundi-Catembo",
+      },
+      {
+        province_id: "68308efdaf02223900052ee8",
+        name: "Cangandala",
+      },
+      {
+        province_id: "68308efdaf02223900052ee8",
+        name: "Cuaba Nzogo",
+      },
+      {
+        province_id: "68308efdaf02223900052ee8",
+        name: "Cunda-Dia-Baze",
+      },
+      {
+        province_id: "68308efdaf02223900052ee8",
+        name: "Luquembo",
+      },
+      {
+        province_id: "68308efdaf02223900052ee8",
+        name: "Malanje",
+      },
+      {
+        province_id: "68308efdaf02223900052ee8",
+        name: "Marimba",
+      },
+      {
+        province_id: "68308efdaf02223900052ee8",
+        name: "Massango",
+      },
+      {
+        province_id: "68308efdaf02223900052ee8",
+        name: "Mucari",
+      },
+      {
+        province_id: "68308efdaf02223900052ee8",
+        name: "Quela",
+      },
+      {
+        province_id: "68308efdaf02223900052ee8",
+        name: "Quirima",
+      },
+    ],
   })
 }
 
