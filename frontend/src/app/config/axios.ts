f568935450145156a4b868569d6709f8ea0fdfb0
Paulods0 <pauloluguenda0@gmail.com>
2025-06-12 10:11:50 +0100

    chore: remove api from axios.config file

--- frontend/src/app/config/axios.ts
+++ frontend/src/app/config/axios.ts
@@ -2,7 +2,6 @@ import axios from "axios"
 
 export default axios.create({
   baseURL: import.meta.env.VITE_APP_API,
-  // baseURL: "https://management-platform-dzsk.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
